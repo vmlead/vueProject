dcc528ea0532842f6b696afba131d3a1c7638053
vivek <vmlead49@gmail.com>
2022-02-25 14:01:36 +0530

    finished

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Trade or View your profile</h1>
+        <h6>You may Save & Load your data</h6>
+        <h6>Click on 'End Day' to begin a new day!</h6>
+        <hr>
+        <p>Your Funds: {{ funds | currency }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed:{
+            funds () {
+                return this.$store.getters.funds;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <app-stocks v-for="stock in stocks" :stock="stock"></app-stocks>
+    </div>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+    import Stock from  './Stocks.vue';
+    export default {
+        computed : {
+            ...mapGetters({
+                stocks: 'stockPortfolio'
+            })
+        },
+         components : {
+            appStocks: Stock
+         }
+    }
+</script>
+
+<style scoped>
+
+</style>
